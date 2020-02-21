1faa74afb2d7625696d6f5854ff61bba6bae8582
Solovejko <solovejko00@mail.ru>
2020-02-22 01:31:38 +0300

    up

---
+++ src/main/java/ru/itmo/java/qwe.java
@@ -0,0 +1,11 @@
+package ru.itmo.java;
+
+public class qwe {
+    public static void main(String[] args) {
+        System.out.println(charCode('a'));
+    }
+
+    static int charCode(char c) {
+        return c;
+    }
+}
